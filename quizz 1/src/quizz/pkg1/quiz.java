df183f7baaab91053f56f5597c09b43e39091592
SilverShades098 <111525725+SilverShades098@users.noreply.github.com>
2022-08-28 22:05:13 +0500

    Add files via upload

---
+++ quizz 1/src/quizz/pkg1/quiz.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package quizz.pkg1;
+/**//**
+ *
+ * @author zohaib computer
+ */
+public class quiz {
+    public static void main(String [] args){
+        System.out.println(args[0]);
+for(int i=1;i<=3;i++) {
+    int count = 0;
+    System.out.println("Muhammad Mubeen");
+    for(int j=1;j<3;j++)
+    {
+        if(i==1||j==2)
+        {
+        System.out.println("Mubeen Muhammad");
+    }
+    }
+
+}
+}
+}
